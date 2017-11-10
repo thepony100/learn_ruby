fce158ea99a1147fe24711ee8b0ecb2bc84da1e0
Pond <waris.aie@pea.co.th>
2017-11-10 15:59:06 +0700

    Add caesar.rb caesar cipher writen in Ruby.

---
+++ caesar.rb
@@ -0,0 +1,20 @@
+def caesar_cipher (text, number)
+	cipher = ''
+	text.each_char do |char|
+		if (char>='a' and char<='z') or (char>='A' and char<='Z')
+			uplow = char<='Z' ? 'A'.ord : 'a'.ord
+			char = (((char.ord - uplow + number)%26)+uplow).chr
+		end
+		cipher += char
+	end
+	cipher
+end
+if __FILE__ == $0
+	print '[c]ipher or [d]ecipher? '
+	cord = gets.chomp
+	print 'enter key [0-25] '
+	key = gets.to_i
+	key = 26 - key if cord=='d'
+	print 'input : '
+	puts 'output : ' + caesar_cipher(gets,key)
+end
