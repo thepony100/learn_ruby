edd0cec5769e0cfd6aded4fe75fdac2dfba14710
Pond <waris.aie@pea.co.th>
2017-11-10 22:06:11 +0700

    Start working on stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,10 @@
+# File: stock_picker.rb
+# Name: Waris Aiemworawutikul
+# Desc: method 'stock_picker' that take in an array of stock prices and return the best day to buy and the best day to sell.
+
+def stock_picker prices
+	prices.each_index do |index|
+		buy_date = index
+
+	end
+
