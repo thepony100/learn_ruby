e57dfe59e727c7bc9f4bfea81468975dd7ff49ee
Pond <waris.aie@pea.co.th>
2017-11-11 13:15:40 +0700

    Finish stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,8 +3,24 @@
 # Desc: method 'stock_picker' that take in an array of stock prices and return the best day to buy and the best day to sell.
 
 def stock_picker prices
-	prices.each_index do |index|
-		buy_date = index
-
+	best_buy_date = nil
+	best_sell_date = nil
+	best_profit = 0
+	prices.each_index do |buy_date|
+		(buy_date+1...prices.length).each do |sell_date|
+			profit = prices[sell_date] - prices[buy_date]
+			if profit > best_profit
+				best_profit = profit
+				best_buy_date = buy_date
+				best_sell_date = sell_date
+			end
+		end
 	end
+	[best_buy_date,best_sell_date]
+end
+
+if __FILE__ == $0
+	print stock_picker([17,3,6,9,15,8,6,1,10])
+
+end
 
